1a46c4746e3eef8c5f2affd6a6bb89fd3f620f99
taqu <taqu2920.dev@gmail.com>
2024-02-17 02:52:56 +0900

    Revert "Initial commit"
    This reverts commit 8dc5e236df0fca21e9c1c05909893a430b62c770.

--- Source/Detox/Private/DetoxTestFixtureActor.cpp
+++
@@ -1,7 +0,0 @@
-﻿#include "DetoxTestFixtureActor.h"
-
-UObject* ADetoxTestFixtureActor::GetParameter_Implementation(const ADetoxTestActor* Target)
-{
-    return 0<Parameters.Num()? Parameters[0] : nullptr;
-}
-
